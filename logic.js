2307534c7d2f4e5c27932a0ac376991195bca497
Maria Quinton <mariaquinton@Marias-MBP.lan1>
2021-01-25 21:06:57 -0600

    fixed bugs

--- logic.js
+++ logic.js
@@ -11,7 +11,7 @@ function createFeatures(earthquakeData) {
     function onEachFeature(feature, layer) {
             layer.bindPopup('<h3>Magnitude: ' + feature.properties.mag + '<h3><h3>Location: ' + feature.properties.place + '</h3><hr><p>' + new Date(feature.properties.time)+ '</p>')
         }
-        function size(magnitude) {
+        function radiusSize(magnitude) {
             return magnitude * 5000;
         }
         function circleColor(magnitude) {
@@ -35,10 +35,12 @@ function createFeatures(earthquakeData) {
             }
         }
 
+
+
 // earthquake funciton
 var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: function(earthquakeData, latlong) {
-        return L.circl(latlong, {
+        return L.circle(latlong, {
             radius: radiusSize(earthquakeData.properties.mag),
             color: circleColor(earthquakeData.properties.mag),
             fillOpacity: 1
@@ -53,18 +55,18 @@ var earthquakes = L.geoJSON(earthquakeData, {
 // set up map
 // Creating map object
 
-function createMap(earthquakes)
+function createMap(earthquakes) {
 
   
     // Define streetmap and darkmap layers
     var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-      "access_token=pk.eyJ1IjoiYXN3YXRoeW0iLCJhIjoiY2plajZjcGk5MDQ3ajJ3bWQ5bTlxY2I0dSJ9._Dq0RpjIWL058qCxH3LmOA");
+      "access_token=pk.eyJ1IjoibXF1aW50b24iLCJhIjoiY2tqcHVqMmd3Njc5MzMxbnk1YmR2cmF3OCJ9.Y6fVKWWqDskeSBSMWlrYlg");
   
     var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1IjoiYXN3YXRoeW0iLCJhIjoiY2plajZjcGk5MDQ3ajJ3bWQ5bTlxY2I0dSJ9._Dq0RpjIWL058qCxH3LmOA");
+    "access_token=pk.eyJ1IjoibXF1aW50b24iLCJhIjoiY2tqcHVqMmd3Njc5MzMxbnk1YmR2cmF3OCJ9.Y6fVKWWqDskeSBSMWlrYlg");
   
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1IjoiYXN3YXRoeW0iLCJhIjoiY2plajZjcGk5MDQ3ajJ3bWQ5bTlxY2I0dSJ9._Dq0RpjIWL058qCxH3LmOA");
+    "access_token=pk.eyJ1IjoibXF1aW50b24iLCJhIjoiY2tqcHVqMmd3Njc5MzMxbnk1YmR2cmF3OCJ9.Y6fVKWWqDskeSBSMWlrYlg");
 
   // Adding tile layer
 var baseMaps = {
@@ -88,3 +90,5 @@ var myMap = L.map('mapid', {
 L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
 }).addTo(myMap);
+
+}
